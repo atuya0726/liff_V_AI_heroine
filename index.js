accd721dce0c9c70245f33a2c3109698d896ace9
atuya <atuya07261008@gmail.com>
2023-06-07 11:40:38 +0900

    test

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
-// liff.init({
-//     liffId: "1661342381-JVVYVXww",
-//   }).then(() => {
-//     // ログインしていない
-//     if (!liff.isLoggedIn()) {
-//       return liff.login()
-//     }
 
-//     return liff.getProfile()
-//       .then(profile => {
-//         window.open(`https://buy.stripe.com/test_7sIeXt7dA0LL8Pm7st?client_reference_id=${profile.userId}`)
-//       })
-//   }).catch(e => {
-//     console.log(e)
-//   })
 const liffId = "1661342381-JVVYVXww"
 window.addEventListener('load', function(){
     liff.init({
         liffId: liffId
-      })
-        .then(() => {
-          console.log('seccess')
+      }).then(() => {
+        console.log('seccess')
+          // ログインしていない
+        if (!liff.isLoggedIn()) {
+            return liff.login()
+        }
+    
+        return liff.getProfile()
+            .then(profile => {
+                window.open(`https://buy.stripe.com/test_7sIeXt7dA0LL8Pm7st?client_reference_id=${profile.userId}`)
+            })
         })
         .catch((err) => {
           console.log('LIFFアプリの初期化に失敗しました', err);
