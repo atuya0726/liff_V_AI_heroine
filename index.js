fee1d92b4b19134c73123aa6f0360f3c2c4860a8
atuya <atuya07261008@gmail.com>
2023-06-07 13:45:29 +0900

    test

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const liffId = "1661342381-JVVYVXww"
 window.addEventListener('load', function(){
+    let lineIdToken;
     liff.init({
         liffId: liffId
       }).then(() => {
@@ -9,14 +10,14 @@ window.addEventListener('load', function(){
         if (!liff.isInClient() && !liff.isLoggedIn()) {
             liff.login({ redirectUri: location.href });
         }
-        console.log("aaaaa")
+        lineIdToken = liff.getIDToken();
         })
         .catch((err) => {
           console.log('LIFFアプリの初期化に失敗しました', err);
         });
 
     liff.ready.then(() =>{
-        liff.openWindow({url:"https://buy.stripe.com/test_7sIeXt7dA0LL8Pm7st"})
+        liff.openWindow({url:"https://buy.stripe.com/test_7sIeXt7dA0LL8Pm7st?client_reference_id=${lineIdToken}"})
     })
 
 
