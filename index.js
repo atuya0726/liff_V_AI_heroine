242a653f96d894de7d385ab5d3fe61e10259f5fa
atuya <atuya07261008@gmail.com>
2023-06-07 11:27:43 +0900

    test

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-liff.init({
-    liffId: "1661342381-JVVYVXww",
-  }).then(() => {
-    // ログインしていない
-    if (!liff.isLoggedIn()) {
-      return liff.login()
-    }
+// liff.init({
+//     liffId: "1661342381-JVVYVXww",
+//   }).then(() => {
+//     // ログインしていない
+//     if (!liff.isLoggedIn()) {
+//       return liff.login()
+//     }
 
-    return liff.getProfile()
-      .then(profile => {
-        window.open(`https://buy.stripe.com/test_7sIeXt7dA0LL8Pm7st?client_reference_id=${profile.userId}`)
-      })
-  }).catch(e => {
-    console.log(e)
+//     return liff.getProfile()
+//       .then(profile => {
+//         window.open(`https://buy.stripe.com/test_7sIeXt7dA0LL8Pm7st?client_reference_id=${profile.userId}`)
+//       })
+//   }).catch(e => {
+//     console.log(e)
+//   })
+
+liff.init({
+    liffId: "1661342381-JVVYVXww"
   })
+    .then(() => {
+      if (!liff.isInClient() && !liff.isLoggedIn()) {
+        window.alert("LINEアカウントにログインしてください。");
+        liff.login({ redirectUri: location.href });
+      }
+      lineIdToken = liff.getIDToken();
+    })
+    .catch((err) => {
+      console.log('LIFFアプリの初期化に失敗しました', err);
+    });
+  
